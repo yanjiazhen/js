5bc0208b10725ca7328004dd74807a51d772a412
yanjiazhen <811739743@qq.com>
2022-01-18 21:01:48 +0800

    实现stringify

--- interview_code/jsonStringify.js
+++ interview_code/jsonStringify.js
@@ -1,44 +1,62 @@
-// TODO还未完整实现
-
-function format(val) {
-    // TODO
-    if (typeof val === "object") {
-        deepObj(val)
-    } else if (typeof val === "string") {
-        return `"${val}"`
-    } else if (typeof val === "number") {
-        return val.toString()
+/* 对象转字符串
+    1、分辨类型，赋值
+    2、便利对象，拼接字符串
+    3、key用双引号封装，value判断是否还有子集有同样再封装，字符串用
+*/
+let str = "";
+function objToStr(obj) {
+  if (typeof obj === "undefined") {
+    str += undefined;
+  } else if (typeof obj === "number") {
+    str += obj;
+  } else if (typeof obj === "string") {
+    str += `"${obj}"`;
+  } else if (typeof obj === "object") {
+    if (obj === null) {
+      str += null;
     }
-}
-// JSON.stringify(obj)
-//"{\"aa\":{\"bb\":\"1231\"}}"
-// 递归便利
-
-function toNum(val){
-    if (typeof val === "string") {
-      return `"${val}"`
-    }else if (typeof val === "number") {
-      return val.toString()
-    }
-}
-
-function deepObj(obj) {
-    newObj = Array.isArray(obj) ? [] : {}
-    if (obj && typeof obj === "object") {
-        for(let key in obj) {
-            if (obj[key] && typeof obj[key] === "object") {
-                newObj = deepObj(obj[key])
-            } else {
-                newObj[key] = obj[key]
-                // 便利赋值
-                if (Array.isArray(obj)) {
-                    newObj += toNum(obj[key])
-                } else {
-                    newObj[key] = `"{\"${key}\":\"obj[key]\"}"`
-                }
-            }
+    if (Array.isArray(obj)) {
+      str += `[`;
+      for (let i in obj) {
+        if (typeof obj[i] === "object") {
+          objToStr(obj[i]);
+        } else {
+          str += `${typeof obj[i] === "number" ? obj[i] : '"' + obj[i] + '"'}`;
+          if (i < obj.length - 1) {
+            str += `,`;
+          }
         }
+      }
+      str += `]`;
+    } else {
+      str += `{`;
+      for (let i in obj) {
+        if (typeof obj[i] === "object") {
+          str += `"${i}":`;
+          objToStr(obj[i]);
+        } else {
+          str += `"${i}":${
+            typeof obj[i] === "number" ? obj[i] : '"' + obj[i] + '"'
+          }`;
+        }
+      }
+      str += `}`;
     }
-    return newObj
+  }
+  return str;
 }
-format([12,324])
+
+console.log(
+  objToStr(undefined),
+  //   objToStr(["123", 123, { aa: "bb" }]),
+  //   objToStr(["123", 123]),
+  //   objToStr(123),
+  //   objToStr("123"),
+  //   objToStr({ aa: [123, { aa: 123 }] }),
+  //   JSON.stringify(["123", 123])
+  //   JSON.stringify(123)
+  //   JSON.stringify("123")
+  //   JSON.stringify({ aa: [123, { aa: 123 }] })
+  JSON.stringify(null)
+  //   JSON.stringify(["123", 123, { aa: "bb" }])
+);
